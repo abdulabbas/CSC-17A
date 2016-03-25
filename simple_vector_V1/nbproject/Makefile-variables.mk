4706d2a445f8eaa8d720b743b75557f7bbd4070b
Abdulhakim Abbas <abdulhabbas@live.com>
2016-03-24 17:11:40 -0700

    Work

--- simple_vector_V1/nbproject/Makefile-variables.mk
+++ simple_vector_V1/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=simple_vector_v1
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/simple_vector_v1
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/simple_vector_v1
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=simplevectorv1.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/simplevectorv1.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/simplevectorv1.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
 CND_ARTIFACT_NAME_Release=simple_vector_v1
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/simple_vector_v1
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/simple_vector_v1
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
 CND_PACKAGE_NAME_Release=simplevectorv1.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/simplevectorv1.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/simplevectorv1.tar
 #
 # include compiler specific variables
 #
